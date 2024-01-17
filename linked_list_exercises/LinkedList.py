333968b459471eea8eedeaa7ad848ee732dd8b15
Vijay Kumar <saroyav3@gmail.com>
2024-01-17 21:28:05 +1300

    writing linked list code

--- linked_list_exercises/LinkedList.py
+++ linked_list_exercises/LinkedList.py
@@ -7,4 +7,11 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def 
+    def is_empty(self):
+        return self.head is None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
