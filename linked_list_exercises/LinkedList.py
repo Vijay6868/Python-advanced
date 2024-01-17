34fdf277fa1e1ac47e83e083a06992568fe3949e
Vijay6868 <saroyav3@gmail.com>
2024-01-18 09:23:00 +1300

    finished creating linked list

--- linked_list_exercises/LinkedList.py
+++ linked_list_exercises/LinkedList.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(slef,data=None):
+    def __init__(self,data):
         self.data = data
         self.next = None
 
@@ -15,3 +15,23 @@ class LinkedList:
         if self.head is None:
             self.head = new_node
             return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end="->")
+            current = current.next
+        print("None")
+
+myList = LinkedList()
+myList.append(1)
+myList.append(2)
+myList.append(3)
+myList.append(4)
+
+myList.display()
+
