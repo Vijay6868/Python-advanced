123dec809b8941226e3a2bb443cc9df3ef56fdfc
Vijay6868 <saroyav3@gmail.com>
2024-01-24 22:11:31 +1300

    Challenge added and solved

--- Linkdein-learning-4311093/Start/docstring.py
+++ Linkdein-learning-4311093/Start/docstring.py
@@ -1,1 +1,14 @@
-print(str.__doc__)
+#print(str.__doc__)
+
+# Demonstrate the use of lambda functions
+
+
+def my_function(arg1, arg2=None):
+    print(arg1, arg2)
+"""
+myfunction(arg1, arg2) -> doesn't do anything
+arg1 = 1st agument
+arg2 = 2nd argument
+"""
+
+print(my_function.__doc__)
