8523ebcee2105a263f2594f527938c6c18be4b49
Vijay6868 <saroyav3@gmail.com>
2024-01-24 16:12:31 +1300

    added new files

--- OOP's/Car/Car.py
+++ OOP's/Car/Car.py
@@ -7,11 +7,4 @@ class Car:
     def displayCarDetails(self):
         print(self.make, self.model, self.year)
 
-class Main:
-    def main(self):
-        car_instance = Car("Toyota", "Camry", "2023")
-        car_instance.displayCarDetails()
 
-if __name__ == "__main__":
-    main_instance = Main()
-    main_instance.main()
