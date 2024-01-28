6f11ce4bb32b435b18b0c1114028b257412831a5
Vijay Kumar <saroyav3@gmail.com>
2024-01-28 22:01:33 +1300

    simple added

---
+++ Linkdein-learning-4311093/Start/__pycache__/simple.py
@@ -0,0 +1,16 @@
+# Simple pattern matching using literal values
+
+x = 0
+
+# Literal patterns are explicit values like integers, strings, Booleans, etc
+match x:
+    case 0:
+        print("Zero")
+    case 1:
+        print("One")
+    case "Zero":
+        print(0)
+    case None:
+        print("Nothing")
+    case _:
+        print("No match")
