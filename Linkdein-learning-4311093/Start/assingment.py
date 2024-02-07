4657f9a0b6fa0b94548739d461a91450eef2d1ad
Vijay Kumar <saroyav3@gmail.com>
2024-02-07 23:07:49 +1300

    working on assingment.py flie

--- Linkdein-learning-4311093/Start/assingment.py
+++ Linkdein-learning-4311093/Start/assingment.py
@@ -9,9 +9,15 @@ x = 5
 print(x)
 
 # TODO: the assignment operator is part of an expression
-
+#x:=10)
+#print(x)
 
 # TODO: The assignment expression is useful for writing concise code
+
+thestr = input("value?")
+while thestr != "exit":
+    print(thestr)
+    thestr = input("value?")
 
 
 # TODO: The walrus operator can help reduce redundant function calls
@@ -21,3 +27,15 @@ val_data = {
     "total": sum(values),
     "average": sum(values) / len(values)
 }
+
+y = 7
+print(y)
+
+val = [10,40,50,90]
+x = len(val)
+valData = {
+    "lenght": (l:=len(val)),
+    "total":  (s:=sum(val)),
+    "average":s/l
+}
+print.pp(valData)
